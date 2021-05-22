a395327efc67d178e0d2ccee1af46da389a4945d
William Lucht <aaronlucht@gmail.com>
2021-05-21 17:33:26 -0700

    spliced the API fetch map temporarily to only display the Weather App. I will need to continue to iterate and change css to dynamically display an image for the repo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,14 +3,14 @@ import React from "react";
 function Projects(props) {
   return (
     <div className="col-xl-12">
-      {props.repo.slice(0, 4).map((projName) => (
-        <div id="work-example-1" className="col-xl-12">
-          <a href={projName.html_url} target="_blank" rel="noreferrer">
-            <div id="work-example-1" className="col-xl-12 boxx">
-              <div id="project-examp-one">{projName.name}</div>
-            </div>
-          </a>
-        </div>
+      {props.repo.slice(29).map((projName) => (
+        // <div id="work-example-1" className="col-xl-12">
+        <a href={projName.html_url} target="_blank" rel="noreferrer">
+          <div id="work-example-1" className="col-xl-12 boxx">
+            <div id="project-examp-one">{/* {projName.name} */}</div>
+          </div>
+        </a>
+        // </div>
       ))}
     </div>
   );
